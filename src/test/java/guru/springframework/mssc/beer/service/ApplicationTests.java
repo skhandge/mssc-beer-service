a8be26f4cdb7852b08dab2e519b39a9a9cf48cc7
admin <admin@host.docker.internal>
2020-04-14 17:39:44 +0530

    Project added

---
+++ src/test/java/guru/springframework/mssc/beer/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.mssc.beer.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

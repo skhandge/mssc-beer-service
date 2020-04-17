72ac0d38af2f7f54fdf9c306fee3e7ecfd93e833
admin <admin@host.docker.internal>
2020-04-18 01:06:07 +0530

    Adding Exception Handling

---
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/MvcExceptionHandler.java
@@ -0,0 +1,22 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class MvcExceptionHandler {
+
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<List> constraintViolationException(ConstraintViolationException ex) {
+		List<String> errorList = new ArrayList<>(ex.getConstraintViolations().size());
+		ex.getConstraintViolations().forEach(error -> errorList.add(error.toString()));
+		return new ResponseEntity<>(errorList,HttpStatus.BAD_REQUEST);
+	}
+}

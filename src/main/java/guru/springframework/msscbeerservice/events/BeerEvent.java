cdaa5c827beb6221b70a7387a0d59795cbdf8f68
admin <skhandge@gmail.com>
2020-04-27 13:23:33 +0530

    Creating New Events and configuring JMS

---
+++ src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
@@ -0,0 +1,18 @@
+package guru.springframework.msscbeerservice.events;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@RequiredArgsConstructor
+@Builder
+public class BeerEvent implements Serializable {
+
+    static final long serialVersionUID = 4942379926527748774L;
+
+    private final BeerDto beerDto;
+}

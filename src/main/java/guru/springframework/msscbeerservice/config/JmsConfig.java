41b96d27bbbaf378944567390b93022fa1a14d22
admin <skhandge@gmail.com>
2020-05-01 20:18:47 +0530

    Adding Order Validation, Listener and sending response back

--- src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
+++ src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
@@ -12,6 +12,8 @@ public class JmsConfig {
 
     public final static String BREWING_REQUEST_QUEUE = "brewing-request";
     public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+    public static final String VALIDATE_ORDER_QUEUE = "validate-order";
+    public static final String VALIDATE_ORDER_RESPONSE_QUEUE = "validate-order-response";
 
     @Bean
     public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper){
